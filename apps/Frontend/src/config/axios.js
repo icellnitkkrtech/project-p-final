17631b40db4a70ab3d448ccdfd3cf1cbeeaa764a
naitik <jainnaitik146@gmail.com>
2025-02-01 11:27:33 +0530

    refactor: simplify axios base URL configuration

--- apps/Frontend/src/config/axios.js
+++ apps/Frontend/src/config/axios.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import { API_CONFIG } from './constants';
+import { API_BASE_URL } from './constants';
 
-const getBaseUrl = () => {
-  if (import.meta.env.PROD) {
-    return API_CONFIG.PROD_URL;
-  }
-  return API_CONFIG.BASE_URL;
-};
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
